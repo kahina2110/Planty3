a2916b61df51f8d32cada30322eba31030e07315
kahina2110 <kahina.aouadi@epitech.eu>
2023-11-18 22:36:19 +0100

    [+] 3 pages faites, manque des petits details comme les images a superposer, des éléments css comme le centrage et commander : bouton, forms etx, responsive 2/3 pages : 80% du projet

---
+++ wordpress/wp-content/themes/theme-enfant/footer.php
@@ -0,0 +1,16 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+
+<?php wp_nav_menu([
+                    'theme_location' => 'footer',
+                    'menu_class' => 'mentions',
+                ]);
+
+
+                ?>
+</body>
+</footer>
+</div>
+</html>
